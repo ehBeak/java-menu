a65b18cb5d5f4b04f22990dfa359bbce4073aacd
eunhee <dmsgml9903@gmail.com>
2023-12-01 18:20:23 +0900

    feat(Coach) : 각 코치가 못 먹는 메뉴가 2개 이하가 되는지 검증하는 로직 구현

--- src/main/java/menu/model/Coach.java
+++ src/main/java/menu/model/Coach.java
@@ -1,7 +1,9 @@
 package menu.model;
 
 import static menu.exception.ErrorMessage.COACH_NAME_LENGTH_NOT_ALLOWED;
+import static menu.exception.ErrorMessage.RESTRICTED_MENU_COUNT_NOT_ALLOWED;
 
+import java.util.List;
 import menu.exception.ExceptionWithMessage;
 
 public class Coach {
@@ -10,15 +12,24 @@ public class Coach {
     private static final int NAME_MIN_LENGTH = 4;
 
     private String name;
+    private List<Menu> restrictedMenus;
 
-    public Coach(String name) {
+    public Coach(String name, List<Menu> restrictedMenus) {
         validateName(name);
+        validateRestrictedMenusSize(restrictedMenus);
         this.name = name;
+        this.restrictedMenus = restrictedMenus;
     }
 
     private void validateName(String name) {
         if (name.length() < NAME_MAX_LENGTH || name.length() > NAME_MIN_LENGTH) {
             throw new ExceptionWithMessage(COACH_NAME_LENGTH_NOT_ALLOWED.toString());
+        }
+    }
+
+    private void validateRestrictedMenusSize(List<Menu> menus) {
+        if (menus.size() > 2) {
+            throw new ExceptionWithMessage(RESTRICTED_MENU_COUNT_NOT_ALLOWED.toString());
         }
     }
 }

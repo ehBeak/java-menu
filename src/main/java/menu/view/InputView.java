2d88d20f0190f5ebb66d2a1acc2e946452e2cafa
eunhee <dmsgml9903@gmail.com>
2023-12-01 18:11:40 +0900

    feat(InputView) : 각 코치가 싫어하는 메뉴 입력하는 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -9,7 +9,7 @@ import menu.util.InputValidator;
 public class InputView {
 
     private static String INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
-
+    private static String INPUT_RESTRICTED_MENU = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
 
     private final InputValidator inputValidator;
 
@@ -20,6 +20,16 @@ public class InputView {
     public List<String> inputCoachName() {
         System.out.println(INPUT_COACH_NAME);
         String input = Console.readLine();
+        inputValidator.validateCoachNamesFormat(input);
+        return new ArrayList<>(Arrays.asList(input.split(",")));
+    }
+
+    public List<String> inputCoachRestrictedMenu(String coachName) {
+        System.out.println(String.format(INPUT_RESTRICTED_MENU, coachName));
+        String input = Console.readLine();
+        if (input.isBlank()) {
+            return new ArrayList<>();
+        }
         inputValidator.validateCoachNamesFormat(input);
         return new ArrayList<>(Arrays.asList(input.split(",")));
     }

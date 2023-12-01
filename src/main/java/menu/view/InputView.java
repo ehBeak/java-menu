e0ad580b3bdc1b95e91cc5919a75b8bb53ec91e9
eunhee <dmsgml9903@gmail.com>
2023-12-01 17:47:11 +0900

    feat(OutputView) : 시작 문구 출력 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -8,7 +8,6 @@ import menu.util.InputValidator;
 
 public class InputView {
 
-    private static String START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
     private static String INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
 
 
---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,10 @@
+package menu.view;
+
+public class OutputView {
+
+    private static String START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+
+    public void startRecommendMenu() {
+        System.out.println(START_MESSAGE);
+    }
+}

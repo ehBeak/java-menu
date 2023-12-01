65f8ea6006751ac0bebe3b931e2efaffe50796de
eunhee <dmsgml9903@gmail.com>
2023-12-01 17:59:16 +0900

    feat(Coaches) : 코치 리스트 생성시, 각 코치가 최소 2명, 최대 5명 사이인지 검증하는 기능 구현

--- src/main/java/menu/exception/ErrorMessage.java
+++ src/main/java/menu/exception/ErrorMessage.java
@@ -4,6 +4,7 @@ public enum ErrorMessage {
 
     INPUT_FORMAT_NOT_ALLOWED("유효하지 않은 입력 형식입니다."),
     COACH_NAME_LENGTH_NOT_ALLOWED("코치 이름은 최소 2글자, 최대 4글자까지 가능합니다."),
+    COACH_SIZE_NOT_ALLOWED("코치는 최소 2명에서 최대 5명까지 입력 가능합니다."),
     ERROR_MESSAGE("[ERROR]");
 
     ErrorMessage(String message) {
---
+++ src/main/java/menu/model/Coaches.java
@@ -0,0 +1,25 @@
+package menu.model;
+
+import static menu.exception.ErrorMessage.COACH_SIZE_NOT_ALLOWED;
+
+import java.util.List;
+import menu.exception.ExceptionWithMessage;
+
+public class Coaches {
+
+    private static final int COACHES_MIN_SIZE = 2;
+    private static final int COACHES_MAX_SIZE = 5;
+
+    private final List<Coach> coaches;
+
+    public Coaches(List<Coach> coaches) {
+        validateCoach(coaches);
+        this.coaches = coaches;
+    }
+
+    private void validateCoach(List<Coach> coaches) {
+        if (coaches.size() < COACHES_MIN_SIZE || coaches.size() > COACHES_MAX_SIZE) {
+            throw new ExceptionWithMessage(COACH_SIZE_NOT_ALLOWED.toString());
+        }
+    }
+}
